bd542a8cb0a948c688e2ac1d791dc6428d57fc02
ashsax <ashwin.222@gmail.com>
2014-03-30 03:07:50 -0400

    updated

--- index.php
+++ index.php
@@ -1,21 +1,39 @@
 <?php
 
 	// process client request (VIA URL)
-	
-	if (!empty($_GET['name']) {
+	header("Content-Type:application/json");
+	include 'function.php';
+	if (!empty($_GET['name'])) {
 		//
 		$name=$_GET['name'];
 		$price=get_price($name);
 		
 		if (empty($price)) {
 			// book not found
+			deliver_response(200,"book not found", NULL);
 		}
 		else {
 			// respond with book price
+			deliver_response(200,"book found",$price);
 		}
 	}
 	else {
 		// throw invalid request
+		deliver_response(400,"Invalid request",NULL);
+	}
+	
+	function get_price($find) {
+		$books = array (
+			"java"=>299,
+			"c"=>348,
+			"php"=>267
+		);
+		foreach($books as $book=>$price) {
+			if ($book == $find) {
+				return $price;
+				break;
+			}
+		}
 	}
 	
 	function deliver_response($status,$status_message,$data) {
@@ -25,7 +43,7 @@
 		$response['status_message'] = $status_message;
 		$response['data'] = $data;
 		
-		$json_response=json_encode($response);
+		$json_response = json_encode($response);
 		echo $json_response;
 	}
 ?>
