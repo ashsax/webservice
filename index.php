675bdb21583515c625fc60b2495b77e4e4982089
ashsax <ashwin.222@gmail.com>
2014-03-30 03:46:26 -0400

    finally working

--- index.php
+++ index.php
@@ -2,6 +2,7 @@
 
 	// process client request (VIA URL)
 	header("Content-Type:application/json");
+	include("function.php");
 	if (!empty($_GET['name'])) {
 		//
 		$name=$_GET['name'];
@@ -19,20 +20,6 @@
 	else {
 		// throw invalid request
 		deliver_response(400,"Invalid request",NULL);
-	}
-	
-	function get_price($find) {
-		$books = array (
-			"java"=>299,
-			"c"=>348,
-			"php"=>267
-		);
-		foreach($books as $book=>$price) {
-			if ($book == $find) {
-				return $price;
-				break;
-			}
-		}
 	}
 	
 	function deliver_response($status,$status_message,$data) {
